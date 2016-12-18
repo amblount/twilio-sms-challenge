a0e27897a01791de23d052fca7135d903147ea77
Alivia Blount <aliviablount@gmail.com>
2016-12-17 19:27:42 -0800

    format new messages page with labels separating email form phone input

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,11 +5,15 @@ class MessagesController < ApplicationController
 
   def create
     action = MessageCreator.new(params)
+    p '!!!!!!!!!!!'
+    p action.message
+    p '!!!!!!!!!!'
     if action.ok?
       flash[:message] = "Message sent"
       redirect_to new_message_path
     else
       @message = action.message
+      p "NO message sorrry"
       render :new
     end
   end
--- app/models/message_creator.rb
+++ app/models/message_creator.rb
@@ -1,26 +1,40 @@
 class MessageCreator
+  require 'twilio-ruby'
   attr_accessor :message, :sms_record
 
   def initialize(params)
     @message = Message.new(allowed_params(params))
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def ok?
-    save_message && send_notification
+    save_message && send_notification && send_message(@message)
+  end
+
+  def send_message(message)
+    # @client.account.messages.create(
+    #   from: message.sender_email,
+    #   to: message.recipient_email,
+    #   body: message.body
+    # )
   end
 
   private
 
   def send_notification
+    #sends an email notification to recipient as an alert of a new message
     MessageMailer.secure_message(@message).deliver_now
   end
 
   def save_message
     @message.secure_id = SecureRandom.urlsafe_base64(25)
     @message.save
+    p 'HERE'
   end
 
   def allowed_params(params)
-    { sender_email: params[:message][:sender], recipient_email: params[:message][:recipient], body: params[:message][:body]}
+    { sender_phone:params[:message][:sender], sender_email: params[:message][:sender], recipient_phone: params[:message][:recipient], recipient_email: params[:message][:recipient], body: params[:message][:body]}
   end
 end
